ab76325f1066ea97c88e36f73cc2aa0e624e1ce7
raulnicolasagusto <raulnicolasagusto@gmail.com>
2025-04-10 15:52:54 -0300

    funciones de registro y login

--- backend/src/controllers/auth/userController.js
+++ backend/src/controllers/auth/userController.js
@@ -1,7 +1,10 @@
 import asyncHandler from "express-async-handler";
 import User from "../../models/auth/UserModel.js";
 import generateToken from "../../helpers/generateToken.js";
+import bcrypt from "bcrypt";
 
+
+// USER REGISTER
 export const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
 
@@ -62,4 +65,59 @@ export const registerUser = asyncHandler(async (req, res) => {
         res.status(400).json({message:"Error al crear el usuario"});
     }
 
+});
+
+//USER LOGIN
+
+export const loginUser = asyncHandler(async (req, res) => {
+    // get email and password from req.body
+    const { email, password } = req.body;
+    //validation
+    if (!email || !password) {
+        //400 bad request 
+        res.status(400).json({message:"Todos los campos son requeridos"});
+        }
+    //check if user exists
+    const userExist = await User.findOne({ email });
+    if (!userExist) {
+        res.status(400).json({message:"El usuario no existe, create un usario para acceder!"});
+    }
+
+    //check if password match the user password
+    const isMatch = await bcrypt.compare(password, userExist.password);
+    if (!isMatch) {
+        res.status(400).json({message:"Contraseña incorrecta"});
+    }
+
+    //generate token with user id
+    const token = generateToken(userExist._id);
+    if (userExist && isMatch) {
+        const { _id, name, email, role, photo, bio, isVerified } = userExist;
+        //set cookie with token
+        res.cookie("token", token, {
+            path: "/",
+            httpOnly: true,
+            maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+            sameSite: true,
+            secure: true,
+        });
+
+        //201 created
+        res.status(200).json({
+            _id,
+            name,
+            email,
+            role,
+            photo,
+            bio,
+            isVerified,
+            token,
+
+            
+        });
+        
+    } else {
+        res.status(400).json({message:"Error al iniciar sesión"});
+    }
+    
 });
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { registerUser } from '../controllers/auth/userController.js';
+import { loginUser } from '../controllers/auth/userController.js';
 
 const router = express.Router();
 
 router.post("/register", registerUser)
+router.post("/login", loginUser);
 
 
 export default router;
