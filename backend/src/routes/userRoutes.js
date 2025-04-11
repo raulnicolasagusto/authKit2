7cdde14d51b2a24b9545b14def866e12047d365f
raulnicolasagusto <raulnicolasagusto@gmail.com>
2025-04-11 15:00:26 -0300

    logout route

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { registerUser } from '../controllers/auth/userController.js';
-import { loginUser } from '../controllers/auth/userController.js';
+import { loginUser, logoutUser } from '../controllers/auth/userController.js';
 
 const router = express.Router();
 
 router.post("/register", registerUser)
 router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+
 
 
 export default router;
