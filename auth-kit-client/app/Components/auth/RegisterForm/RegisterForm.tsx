2f19f6e9a16458b115c4ddc5634b18b15fa10e51
raulnicolasagusto <raulnicolasagusto@gmail.com>
2025-04-30 18:22:15 -0300

    seguimos con register

--- auth-kit-client/app/Components/auth/RegisterForm/RegisterForm.tsx
+++ auth-kit-client/app/Components/auth/RegisterForm/RegisterForm.tsx
@@ -18,7 +18,7 @@ function RegisterForm  () {
           </p>
           
           <div className="flex flex-col">
-            <label htmlfor="name" className="mb-1 text-[#999]">
+            <label htmlFor="name" className="mb-1 text-[#999]">
              Full Name
             </label>
             <input
@@ -29,7 +29,7 @@ function RegisterForm  () {
           </div>
          
                 <div className="mt-[1rem] flex flex-col">
-                 <label htmlfor="email" className="mb-1 text-[#999]">
+                 <label htmlFor="email" className="mb-1 text-[#999]">
                   Email Adress
                  </label>
                  <input
@@ -40,20 +40,33 @@ function RegisterForm  () {
                </div>
             
                <div className="relative mt-[1rem] flex flex-col">
-               <label htmlfor="password" className="mb-1 text-[#999]">
+               <label htmlFor="password" className="mb-1 text-[#999]">
                 Password
                </label>
                <input
                  type="password"
                  id="password"
                  className="px-4 py-3 border-[2px] rounded-md outline-[#2ECC71] text-gray-800"
-                 placeholder="*****"/>
+                 placeholder="*****"
+                 />
                  <button className="absolute p.1 right-4 top-[43%] text-[22px] text-[#999] opacity-45">
                   <i className="fas fa-eye"></i>
                  </button>
              </div>
-
-
+             <div className="mt-4 flex justify-end">
+              <a
+              href="/forgot-password"
+              className="font-bold text-[#2ECC71] text-[14px] hover:text-[#7263f3] transition-all duration-300"
+              >
+              Forgot password?
+              </a>
+             </div>
+             <div className="felx">
+              <button type="Submit" className="mt-[1.5rem] flex-1 px-4 py-3 font-bold bg-[#2ECC71] text-white rounded-md hover:bg-[#1abc9c] transition-colors">
+                Register Now
+              </button>
+             </div>
+// seguir video 3:18:20
       </div>
     </form>
   )
--- auth-kit-client/app/layout.tsx
+++ auth-kit-client/app/layout.tsx
@@ -28,8 +28,9 @@ export default function RootLayout({
       <head>
         
         <link rel="stylesheet"
-         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" 
-         
+         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+          crossOrigin="anonymous" 
+          referrerPolicy="no-referrer"    
          />
           
         
